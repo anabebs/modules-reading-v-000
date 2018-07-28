16bee6637f75806c89f0e148c8c9de9b1e0a6376
Ana Bebic <anabebic.x@gmail.com>
2018-07-28 15:37:37 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,6 +3,8 @@ require_relative './class_methods_module.rb'
 
 class Kid
   
- extend FancyDance::ClassMethods
-  include FancyDance::InstanceMethods
+ extend 
+ FancyDance::ClassMethods
+  include
+  FancyDance::InstanceMethods
 end
